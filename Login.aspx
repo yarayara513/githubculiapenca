2e9070d59a33e61906e33805621b833ec4c655bd
Choroguarda <81122466+Choroguarda@users.noreply.github.com>
2021-03-21 21:48:05 -0300

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="TIPO1.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h3>Inicio Sesion
+        </h3>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label">Bienvenido/a</asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Label">Nombre de usuario: </asp:Label>
+            <asp:TextBox ID="txtusuario" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Label">Contraseña: </asp:Label>
+            <asp:TextBox ID="txtpass" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btningresar" runat="server" Text="Ingresar" OnClick="Button1_Click" />
+            <asp:Button ID="btnregistar" runat="server" Text="Registrarse"/>
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TIPO1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        profealtokeEntities bd = new profealtokeEntities();
+        USUARIO usuario;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            int inicio = 2;
+            Boolean encontrado = false;
+
+            while(encontrado == false)
+            {
+                string usu;
+                string password;
+                usu = bd.USUARIO.Find(inicio).NOMBRE_USUARIO;
+                password = bd.USUARIO.Find(inicio).CONTRASENA;
+               
+                if ( txtusuario.Text.Equals(usu) && txtpass.Text.Equals(password))
+                {                
+                    encontrado = true;
+                    Response.Redirect("Perfil.aspx?ID="+inicio);                 
+                }
+                else{
+                    inicio++;
+                    Response.Redirect("Login.aspx?");
+                }
+ 
+            }
+        }
+    }
+}
